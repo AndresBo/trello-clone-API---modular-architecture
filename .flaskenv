64e2d079bd1307934c85d4ab017c3e5e445fe0b5
AndresBo <anndresfborrero@gmail.com>
2023-02-07 10:55:47 +1000

    modified main.py

--- .flaskenv
+++ .flaskenv
@@ -1,4 +1,4 @@
 # contains information to tell flask where to find our app files and how to run our project. 
 
-FLASK_APP=main
+FLASK_APP=main:create_app
 FLASK_ENV=development
--- main.py
+++ main.py
@@ -1,9 +1,17 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
+def create_app():
+    # using a list comprehension and multiple assignment 
+    # to grab the environment variables we need
+    
+    # Creating the flask app object - this is the core of our app!
+    app = Flask(__name__)
 
-app = Flask(__name__)
+    # configuring our app:
+    app.config.from_object("config.app_config")
 
-
-@app.route('/')
-def hello_world():
-    return 'Hola, mundo'
+    # creating our database object! This allows us to use our ORM
+    db = SQLAlchemy(app)
+    
+    return app
